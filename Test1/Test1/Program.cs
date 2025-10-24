e215207fe7525deca166c8a112607ea94ad6ec1b
Raul Iacob <137762504+iacobraul@users.noreply.github.com>
2025-10-24 20:18:11 +0300

    test 1

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=5, y=10;
+            int a = x * y;
+            Console.WriteLine(a);
+        }
+    }
+}
