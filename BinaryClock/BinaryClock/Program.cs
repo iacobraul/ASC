ce4c7fb209186dd3992ff8b8e5c7f237986e554e
Raul Iacob <137762504+iacobraul@users.noreply.github.com>
2025-12-29 13:25:13 +0200

    Binary Clock

---
+++ BinaryClock/BinaryClock/Program.cs
@@ -0,0 +1,21 @@
+﻿//Scrieti un program C# care afiseaza in timp real un ceas digital. 
+
+while(true)
+{   
+    DateTime now = DateTime.Now;
+    string h1 = Convert.ToString((now.Hour)/10 , 2).PadLeft(4,'0');
+    string h2 = Convert.ToString((now.Hour)%10 , 2).PadLeft(4,'0');
+    string m1 = Convert.ToString((now.Minute)/10 , 2).PadLeft(4,'0');
+    string m2 = Convert.ToString((now.Minute)%10 , 2).PadLeft(4,'0');
+    string s1 = Convert.ToString((now.Second)/10 , 2).PadLeft(4,'0');
+    string s2 = Convert.ToString((now.Second)%10 , 2).PadLeft(4,'0');
+    Console.Clear();
+
+    Console.WriteLine($"{ now: HH: mm: ss}");
+    
+    for(int i=0;i<=3;i++)
+    {
+        Console.WriteLine($"{8/Math.Pow(2,i)} | {h1[i]} {h2[i]} | {m1[i]} {m2[i]} | {s1[i]} {s2[i]} ");
+    }
+    Thread.Sleep(1000);
+}
